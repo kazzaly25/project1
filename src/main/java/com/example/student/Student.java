c65539004be284c5459ce820919c3e2b0bd80f64
kazzaly25 <kazzalymohamed@gmail.com>
2022-11-22 13:29:44 +0530

    first commit

---
+++ src/main/java/com/example/student/Student.java
@@ -0,0 +1,59 @@
+package com.example.student;
+
+import javax.persistence.Entity;
+@Entity
+public class Student {
+	
+	private int rollno;
+	private String name;
+	private int phoneno;
+	
+    public Student() {}
+	
+	public Student(String name, int phoneno) {
+		this.setName(name);
+		this.setPhoneno(phoneno);
+	}
+	
+	public Student(int rollno, String name,int phoneno) {
+		this.setRollno(rollno);
+		this.setName(name);
+		this.setPhoneno(phoneno);
+	}
+	
+	
+	
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhoneno() {
+		return phoneno;
+	}
+
+	public void setPhoneno(int phoneno) {
+		this.phoneno = phoneno;
+	}
+
+	
+	@Override
+	public String toString() {
+		System.out.println("Student{" + "rollno=" + rollno +",name='"+ name +"'" +", phoneno='"+ phoneno + "'"+'}');
+		return "Student{" + "rollno=" + rollno +",name='"+ name +"'" +", phoneno='"+ phoneno + "'"+'}';
+	}
+		
+}
+
+
